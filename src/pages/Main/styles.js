63ecb9a085bcca17073187564d9823be674a8e5b
lennonconstantino <lennonconstantino@gmail.com>
2025-06-24 16:52:14 -0300

    listando repositorios

--- src/pages/Main/styles.js
+++ src/pages/Main/styles.js
@@ -43,7 +43,7 @@ from{
 to{
     transform: rotate(360deg);
 }
-`
+`;
 
 export const SubmitButton = styled.button.attrs(props => ({
     type: 'submit',
@@ -72,3 +72,34 @@ ${props => props.loading &&
 }
 `;
 
+export const List = styled.ul`
+list-style: none;
+margin-top: 20px;
+li {
+    padding: 15px 0;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+
+    & + li {
+        border-top: 1px solid #eee;
+    }
+
+    a {
+        color: #0D2636;
+        text-decoration: none;
+    }
+}
+`;
+
+export const DeleteButton = styled.button.attrs({
+    type: 'button'
+})`
+margin-left: 6px;
+background: transparent;
+color:#0D2636;
+border:0;
+padding: 8px 7px;
+outline: 0;
+border-radius: 4px;
+`;
